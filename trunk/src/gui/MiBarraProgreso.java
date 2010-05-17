a7e3aef49b255b81a703138d8f58f6a3723396a4
frannavas1984 <frannavas1984@42e5e398-df54-5f7f-faea-49c22571d92c>
2010-05-17 14:13:55 +0000

    Actualizada la interfaz de usuario.

---
+++ trunk/src/gui/MiBarraProgreso.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui;
+
+import java.awt.Component;
+import javax.swing.JProgressBar;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+/**
+ * Clase que redefine el comportamiento de la tabla en su tercera columna, es
+ * decir, la columna que contiene la barra de progreso
+ * @author sema
+ */
+public class MiBarraProgreso implements TableCellRenderer {
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                    boolean isSelected, boolean hasFocus, int row, int column) {
+            return (JProgressBar) value;
+    }
+}
